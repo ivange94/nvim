dbbc3bf381edb32c922fbb4325140e46aa9e425e
Ivange Larry <ivangelarry@gmail.com>
2021-12-24 11:24:00 +0100

    Add autopairs plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "lunarvim/darkplus.nvim" -- colorscheme
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
