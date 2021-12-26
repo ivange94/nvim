40a41b811c951d26be272ddcb9501bd6e81cb943
Ivange Larry <ivangelarry@gmail.com>
2021-12-26 07:57:20 +0100

    Update dashboard

--- lua/user/dashboard.lua
+++ lua/user/dashboard.lua
@@ -18,8 +18,9 @@ vim.g.dashboard_custom_header = {
 
 vim.g.dashboard_custom_section = {
     a = {description = {'  Find File          '}, command = "Telescope find_files"},
-    d = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
-    b = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
-    e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'},
+    b = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
+    c = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
+    d = {description = {'  Recent Projects    '}, command = 'Telescope projects'},
+    e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'}
 }
 vim.g.dashboard_custom_footer = {'Talk is cheap. Show me the code. - Linus Torvalds'}
