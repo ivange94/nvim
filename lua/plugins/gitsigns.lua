4e3932938201baafe72c9bb0c51025740fdb72a3
Larry <ivangelarry@gmail.com>
2026-02-21 22:41:49 -0600

    show git blame for line

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 500,
+      virt_text_pos = "eol",
+    },
+    current_line_blame_formatter = "<author> ∙ <summary> ∙ <author_time>",
+  },
+}
